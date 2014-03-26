3324dddc9306bbb4781828941cb26c2baca81a08
sigvartmh <sigvart.m@gmail.com>
2014-03-26 02:54:20 +0100

    Working with shell call for mergeing
    Todo: Make dependencies.txt
    Works now also puts in Trinn, Antall timer Sats, and should also be able
    to sum hours and pay to get the total sum. Going to add functions for
    autofill all that is needed

--- test.py
+++ test.py
@@ -4,6 +4,14 @@
 #import os
 #import subprocess
 
+"""
+	Todo:
+		1. Do a check with os if the previous file exist and do something to avoid crash if 2 people compile their list at the same time.
+		2. Improve api
+		3. Clean code
+		4. Uncomment subprocess and subprocess.call(f(x)) when on server to do pdf+fdf merge.
+"""
+
 def fdf_txt_object(field_name, field_value):
 	fdf_obj = "<</T("+str(field_name)+")/V("+str(field_value)+")>>"
 	return fdf_obj
@@ -11,13 +19,18 @@ def fdf_txt_object(field_name, field_value):
 #filename = "output.pdf"
 #filename should be the same as the pdf you are going to merge the data with
 
-def fdf_make(filename, fdf_list):
+def fdf_make(filename, fdf_list,lonns_trinn):
+#Meta header for FDF	
 	fdf = ['%FDF-1.2\n']
 	fdf.append('%âãÏÓ\n')
 	fdf.append('1 0 obj\n')
 	fdf.append('<</FDF<</F('+filename+')/Fields[')
-	i=1;
-	j=1;
+	
+#initialize values
+	i=1
+	j=1
+	sum = 0
+	
 	for x in range(0,len(fdf_list)):
 		if fdf_list[x][0] == "uke":
 			name  = "Uke nrRow"+str(i)
@@ -27,10 +40,16 @@ def fdf_make(filename, fdf_list):
 		elif fdf_list[x][0] == "time":
 			name  = "pr ukeRow"+str(j)
 			value = fdf_list[x][1]
+			sum = sum + value
 			fdf.append(fdf_txt_object(name,value))
 			j=j+1
+	fdf.append(fdf_txt_object("Sum",sum))
+	fdf.append(fdf_txt_object("Antall timer",sum))
+	fdf.append(fdf_txt_object("Sats",lonns_trinn[1]))
+	fdf.append(fdf_txt_object("Trinn",lonns_trinn[0]))
+	fdf.append(fdf_txt_object("Belop",lonns_trinn[1]*sum))
 	
-
+#Meta end of file for FDF
 	fdf.append(']/UF('+filename+')>>/Type/Catalog>>')
 	fdf.append('endobj\n')
 	fdf.append('trailer\n')
@@ -45,10 +64,16 @@ def fdf_make(filename, fdf_list):
 
 	proccess_string='pdftk '+filename+' fill_form output.fdf output output.pdf'
 
-	a = subprocess.check_output(proccess_string, shell=True)
+	#Danger shell=True is a security hazard try shell=false
+	#subprocess.call(proccess_string, shell=True)
+
+valid_strings=["Sats","Antall timer","time","uke","Trinn"]
+#if in valid strings do fdf.append(fdf_txt_object(name,value))
 
 mylist=[("uke",3),("time",12),("time",14),("time",13),("uke",4),("uke",5)]
-fdf_make("P_6_web.pdf",mylist)
+lonns_trinn=["C23",154.73]
+
+fdf_make("P_6_web.pdf",mylist, lonns_trinn)
 
 """
 filename = "P_6_web.pdf"
